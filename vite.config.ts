20ba52566502cc76de65f4284404b49971ad37db
Niek <frankenstein795@gmail.com>
2023-03-22 20:26:29 +0100

    Add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { fileURLToPath, URL } from "node:url";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), basicSsl()],
+    plugins: [
+        vue(),
+        basicSsl(),
+        VitePWA({ registerType: "autoUpdate" })
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url))
