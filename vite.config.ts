e0937535dde67f1c3e6a7a85cfc1c72f235d033e
Niek <frankenstein795@gmail.com>
2023-03-22 20:47:56 +0100

    Add PWA configs, add generic favicons

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,40 @@ export default defineConfig({
     plugins: [
         vue(),
         basicSsl(),
-        VitePWA({ registerType: "autoUpdate" })
+        VitePWA({
+      mode: "development",
+      base: "/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
+      manifest: {
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    })
     ],
     resolve: {
         alias: {
